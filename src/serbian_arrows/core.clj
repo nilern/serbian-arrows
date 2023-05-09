da7396854bd9d8a86d2b44673c7fcb6cb36f393f
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2023-05-09 23:49:22 +0300

    Add `->>`

--- src/serbian_arrows/core.clj
+++ src/serbian_arrows/core.clj
@@ -1,6 +1,27 @@
-(ns serbian-arrows.core)
+(ns serbian-arrows.core
+  (:refer-clojure :exclude [->>]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defmulti expand-sink (fn [callee _ _ _] callee))
+
+
+(defmethod expand-sink 'sequence [_ _ xform seq-form]
+  `(sequence ~xform ~seq-form))
+
+
+(defmethod expand-sink 'into [_ args xform seq-form]
+  `(into ~@args ~xform ~seq-form))
+
+
+(defmethod expand-sink 'eduction [_ _ xform seq-form]
+  `(eduction ~xform ~seq-form))
+
+
+(defmethod expand-sink 'reduce [_ args xform seq-form]
+  `(transduce ~xform ~@args ~seq-form))
+
+
+(defmacro ->> [seq-form & forms]
+  (let [xforms (butlast forms)
+        [callee & args] (last forms)]
+    (expand-sink callee args `(comp ~@xforms) seq-form)))
